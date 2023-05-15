d1028f9606cecb6958c824823d964357af841245
Jacob Hargraves <jacobhargraves1@gmail.com>
2023-05-15 10:41:20 -0600

    added js to update the class based on the current hour

--- assets/script.js
+++ assets/script.js
@@ -10,10 +10,31 @@ $(function() {
   // useful when saving the description in local storage?
   //
   // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
+  // block by comparing the id to the current hour
+
+  // updates currentHour every minute
+  setInterval(currentHour, 60000)
+  // gets current hour in 24-hour format and reads it as an integer
+  var currentHour = parseInt(dayjs().format('H'),10);
+  console.log(currentHour);
+  // loops through each time block element
+    $('.time-block').each(function() {
+      // Gets the ID of the current time block
+      const blockId = $(this).attr('id');
+      
+      // Extracts the hour from the ID and reads as an integer
+      const blockHour = parseInt(blockId.split('-')[1],10);
+      
+      // Compare the block hour with the current hour and adds or removes a class based on 
+      // the comparison
+      if (blockHour < currentHour) {
+        $(this).addClass('past').removeClass('present future');
+      } else if (blockHour === currentHour) {
+        $(this).addClass('present').removeClass('past future');
+      } else if (blockHour > currentHour) {
+        $(this).addClass('future').removeClass('past present');
+      }
+    });
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
@@ -27,5 +48,5 @@ $(function() {
     var min = dayjs().minute();
     var sec = dayjs().second();
     $('#currentDay').text(today.format('MMM D, YYYY') + " at " + hour + ":" + min + ":" + sec);
-}
+  }
 });
